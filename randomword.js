fd8174ab1a622d27646c63ed4d74d35adfe63723
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-02-15 11:44:11 -0500

    updated print function

--- randomword.js
+++ randomword.js
@@ -1,19 +1,14 @@
 //need to install 'npm i random-part-of-speech'
-
 const rpos = require('random-part-of-speech');
  
 //print random nouns (six) for user to choose
-rpos.getNouns(6).then(response => { console.log(response) });
-# => ['train', 'hook', 'stove', 'cat', 'rice', 'dog']
+rpos.getNouns(6).then(response => ['train', 'hook', 'stove', 'cat', 'rice', 'dog']
 
 //print random adjectives (six) for user to choose
-rpos.getAdjectives(6).then(response => { console.log(response) });
-# => ['scrawny', 'enchanting', 'mute', 'bored', 'weak', 'cloudy']
+rpos.getAdjectives(6).then(response => ['scrawny', 'enchanting', 'mute', 'bored', 'weak', 'cloudy']
 
 //print random adverbs (six) for user to choose
-rpos.getAdverbs(6).then(response => { console.log(response) });
-# => ['currently', 'upright','longingly','woefully','freely','vastly']
+rpos.getAdverbs(6).then(response => ['currently', 'upright','longingly','woefully','freely','vastly']
 
 //print random verbs (six) for user to choose
-rpos.getVerbs(6).then(response => { console.log(response) });
-# => ['screw', 'listen', 'doubt', 'attempt', 'approve', 'rob']
+rpos.getVerbs(6).then(response => ['screw', 'listen', 'doubt', 'attempt', 'approve', 'rob']
