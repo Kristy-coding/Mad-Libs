7236eee9f85bf0480db7c2cb3d9a568261727c2f
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-02-16 13:49:35 -0500

    added random speech functions

--- randomword.js
+++ randomword.js
@@ -1,14 +1,80 @@
 //need to install 'npm i random-part-of-speech'
 const rpos = require('random-part-of-speech');
- 
-//print random nouns (six) for user to choose
-rpos.getNouns(6).then(response => ['train', 'hook', 'stove', 'cat', 'rice', 'dog']
+const fs = require('fs');
+const seekableStream = require('fs-readstream-seek');
 
-//print random adjectives (six) for user to choose
-rpos.getAdjectives(6).then(response => ['scrawny', 'enchanting', 'mute', 'bored', 'weak', 'cloudy']
+const VERB = "verb", NOUN = "noun", ADVERB = "adverb", ADJECTIVE = "adjective", ANY = "any";
 
-//print random adverbs (six) for user to choose
-rpos.getAdverbs(6).then(response => ['currently', 'upright','longingly','woefully','freely','vastly']
+//functions
+module.exports = {
+	getVerbs: (amount = 1) => getWord(VERB, amount),
+	getNouns: (amount = 1) => getWord(NOUN, amount),
+	getAdverbs: (amount = 1) => getWord(ADVERB, amount),
+	getAdjectives: (amount = 1) => getWord(ADJECTIVE, amount),
+};
 
-//print random verbs (six) for user to choose
-rpos.getVerbs(6).then(response => ['screw', 'listen', 'doubt', 'attempt', 'approve', 'rob']
+//this produces a data file path
+function getPathOfType(type) {
+	if(type = [NOUN, VERB, ADVERB, ADJECTIVE][randomInt(0, 3)]); {
+	return `${__dirname}/word_files/${type}s.words`;
+}
+
+//returns a random word
+function getWord(type, amount) {
+	return new Promise((resolveMain, rejectMain) => {
+		if(amount <= 0) {
+			resolveMain([]);
+			return;
+		}
+
+		try {
+			//open the desired file as a stream to freely seek around it
+			const data_path = getPathOfType(type);
+			const file_size = fs.stat(data_path, (err, stats) => {
+				if(err) {
+					rejectMain(err);
+					return;
+				}
+				var data_stream = new seekableStream(data_path);
+				data_stream.setEncoding('utf8');
+
+				//grab a random word from the stream `amount` times
+				var promise_array = [];
+				for (var i = amount - 1; i >= 0; i--) {
+
+					promise_array.push(
+						() => new Promise((resolve, reject) => {
+							data_stream.seek(randomInt(0, stats.size - 1));
+							data_stream.on('data', chunk => {
+								chunk = chunk.split("\n");
+								if(chunk.length < 2) {
+									data_stream.seek(randomInt(0, stats.size));
+									return;
+								}
+								data_stream.removeAllListeners('data');
+								resolve(chunk[1]);
+								return;
+							});
+						})
+					);
+
+				}
+				promise_array.reduce((chain, nextPromiseFunc) => {
+					return chain.then(word_array => {
+						return nextPromiseFunc().then(last_word => {
+							return [...word_array, last_word];
+						});
+					});
+				}, Promise.resolve([]))
+				.then(word_array => {
+					data_stream.destroy();
+					resolveMain(word_array);
+				});
+			});
+		} catch(err) {
+			rejectMain(err);
+			return;
+		}
+	});
+}
+
